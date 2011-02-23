6faaafb0eef0f094bc4552793449c42ae2c48823
svp <svp@svps-Mac.local>
2011-02-23 22:34:39 +0530

    initial push (rails skeleton))

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmspress::Application.config.secret_token = '49c5c326fe67bc07e1f41a056b25c9e214a0a93ec9caf6e79601d94924234b730af77a3ec9e2186145c30e1df21fcc5363ec6a27e54760cef0c033718f071eae'
